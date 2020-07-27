8e9cae493f9d5f7d2b3456e67b23ca1b953a3be4
takanamir <rtakanami0706@gmail.com>
2020-07-27 17:29:55 +0900

    テンプレートエンジンを使いこなす

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.model.DataObject;
+
 @Controller
 public class HelloController {
 	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
+		mav.addObject("msg", "message 1<hr/>message 2<br/>message 3");
 		return mav;
 	}
 }
